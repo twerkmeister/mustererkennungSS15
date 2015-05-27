c411c4aa3417ec72a9c5ad106cb6097b3dabca78
Thomas Werkmeister <thomas.werkmeister@scalableminds.com>
2015-05-27 09:59:48 +0200

    intializing cluster centers with points from the samples

--- exercise3/ex3-3.m
+++ exercise3/ex3-3.m
@@ -1,10 +1,13 @@
 warning ("off", "Octave:broadcast");
 
 clusterData = load("../data/clusters.txt");
+nRows = rows(clusterData);
+
+randomIndex= randperm(nRows);
 
 k = 3;
 
-clusterCenters = unifrnd(0.0, 100.0, k, 2);
+clusterCenters = clusterData(randomIndex(1:k),:);
 
 for i = 1:6
 	distances = cell2mat(arrayfun(@(n) norm(clusterData - clusterCenters(n,:), 2, "rows"), 1:k, "UniformOutput", false));
@@ -16,6 +19,7 @@ for i = 1:6
 	voronoi(clusterCenters(:,1), clusterCenters(:,2));
 	xlim(p, [0.0, 100.0]);
 	ylim(p, [0.0, 100.0]);
+	title(sprintf("iteration %d",i));
 	hold off;
 
 	clusterCenters = cell2mat(arrayfun(@(n) mean(clusterData(clusterIndices==n,:))', 1:k, "UniformOutput", false))';
