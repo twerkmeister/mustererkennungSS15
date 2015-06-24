f6b5bd60e17eafa866faa10c6cc1374f6bf8264c
Johannes Frohnhofen <johannes@frohnhofen.com>
2015-06-24 11:33:19 +0200

    finished ex8

--- exercise8/ex8.m
+++ exercise8/ex8.m
@@ -21,16 +21,29 @@ function sample = sphereSample()
   until length <= 1
 end
 
-samples = arrayfun(@sphereSample, [1:1000], "UniformOutput", false);
+samples = arrayfun(@sphereSample, [1:10000], "UniformOutput", false);
 sampleMat = cell2mat(samples');
+subplot(2,2,1);
 scatter3(sampleMat(:,1), sampleMat(:,2), sampleMat(:,3));
 
 #c
 
 functions = cellfun(@functionNumber, samples);
 histdata = arrayfun(@(n) sum(functions == n), [0:15]) / length(samples);
+subplot(2,2,3);
 bar([0:15], histdata);
 ratio = max(histdata) / min(histdata(histdata > 0))
 
 #d
 
+function f = functionNumberNegative(w)
+  bias = 1;
+  tupels = [-1,-1,bias; -1,1,bias; 1,-1,bias; 1,1,bias]';
+  f = (w*tupels >= 0) * [1,2,4,8]';
+end
+
+functions = cellfun(@functionNumberNegative, samples);
+histdata = arrayfun(@(n) sum(functions == n), [0:15]) / length(samples);
+subplot(2,2,4);
+bar([0:15], histdata);
+ratio = max(histdata) / min(histdata(histdata > 0))
